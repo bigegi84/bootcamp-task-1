f6f4b2900386f3c2165865168f32469a7d02b5b7
bigegi84 <gilangpratamaw@gmail.com>
2021-12-24 13:13:13 +0700

    task 1

--- SampleRESTAPI/Models/Author.cs
+++ SampleRESTAPI/Models/Author.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleRESTAPI.Models
 {
     public class Author
     {
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
+        [Required]
         public DateTime DateOfBirth { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string MainCategory { get; set; }
     }
 }
